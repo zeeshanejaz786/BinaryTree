011c1ded1747b9589d545fa0440552b0195142b9
TOMMY-QURESHI <qureshitricky@gmail.com>
2022-06-16 23:33:10 +0500

    Add project files.

---
+++ BinaryTree/Tree.cs
@@ -0,0 +1,60 @@
+﻿namespace BinaryTree
+{
+    public class Tree<TItem> where TItem : IComparable<TItem>
+    {
+        public TItem NodeData { get; set; }
+        public Tree<TItem>? LeftTree { get; set; }
+        public Tree<TItem>? RightTree { get; set; }
+
+
+        public Tree(TItem nodeValue)
+        {
+            this.NodeData = nodeValue;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+
+        public void Insert(TItem newItem)
+        {
+            TItem currentNodeValue = this.NodeData;
+            if (currentNodeValue.CompareTo(newItem) > 0)
+            {
+                if (this.LeftTree is null)
+                {
+                    this.LeftTree = new Tree<TItem>(newItem);
+                }
+                else
+                {
+                    this.LeftTree.Insert(newItem);
+                }
+
+            }
+            else
+            {
+                if (this.RightTree is null)
+                {
+                    this.RightTree = new Tree<TItem>(newItem);
+                }
+                else
+                {
+                    this.RightTree.Insert(newItem);
+                }
+            }
+        }
+
+        public string WalkTree()
+        {
+            string result = "";
+            if (this.LeftTree is not null)
+            {
+                result = this.LeftTree.WalkTree();
+            }
+            result += $" {this.NodeData.ToString()} ";
+            if (this.RightTree is not null)
+            {
+                result += this.RightTree.WalkTree();
+            }
+            return result;
+        }
+    }
+}
---
+++ BinaryTreeTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using BinaryTree;
+
+
+
+    Tree<int> tree1 = new Tree<int>(100);
+    tree1.Insert(50);
+    tree1.Insert(110);
+    tree1.Insert(-5);
+    tree1.Insert(-12);
+    tree1.Insert(145);
+    tree1.Insert(03);
+    tree1.Insert(14);
+    tree1.Insert(-8);
+    tree1.Insert(160);
+    tree1.Insert(86);
+    tree1.Insert(38);
+    string sortedData = tree1.WalkTree();
+    Console.WriteLine($"The Data After Sorthing is: {sortedData}");
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+
+Tree<string> tree2 = new Tree<string>("Hello Students");
+tree2.Insert("Zeeshan,");
+tree2.Insert("fawad,");
+tree2.Insert("Usman,");
+tree2.Insert("Raza");
+tree2.Insert("Today");
+tree2.Insert("I");
+tree2.Insert("Hope");
+tree2.Insert("You");
+tree2.Insert("Are");
+tree2.Insert("Feeling");
+tree2.Insert("Well");
+tree2.Insert("!");
+sortedData = tree2.WalkTree();
+Console.WriteLine($"Sorted data is: {sortedData}");
+
+
